bfc9717995f5b3d235bd2cca77ade55ea20f7c93
lauriciodev <developerwebmasterpro@gmail.com>
2024-06-02 16:36:18 -0300

    estruturando pastas

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Agendamento</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=0.1">
+</head>
+<body>
+<?php
+echo "<h3>lauricio</h3>";
+
+?>
+<h2>teste apenas</h2>
+</body>
+</html>
